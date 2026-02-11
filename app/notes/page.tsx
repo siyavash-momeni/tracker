cee3dfbc9e315f6111d233d48bbe98befc1865b0
siyavash-momeni <257584617+siyavash-momeni@users.noreply.github.com>
2026-02-11 14:14:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/notes/page.tsx
@@ -0,0 +1,15 @@
+export default function NotePage() {
+    return (
+      <div style={{ 
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        height: "80vh", 
+        fontFamily: "inherit", 
+        fontSize: "1.5rem" 
+      }}>
+        ⚠️ 🚧 Page en constructionconstruction 🚧  ⚠️
+      </div>
+    );
+  }
+  
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black font-sans">
+      <main className="text-center p-8 bg-white dark:bg-zinc-900 rounded-lg shadow-md">
+        <h1 className="text-3xl font-bold text-black dark:text-white mb-4">
+          Bienvenue sur TrackerFront
+        </h1>
+        <p className="text-lg text-zinc-700 dark:text-zinc-300">
+          C'est pour bientot
+        </p>
+      </main>
+    </div>
+  );
+}
