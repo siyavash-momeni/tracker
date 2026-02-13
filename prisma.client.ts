d431761bbb299fc78f9e25e10cd3a533f2e8be63
siyavash-momeni <siyavash.mmn@gmail.com>
2026-02-13 15:22:57 +0100

    singleton pour eviter une repetition

--- prisma.client.ts
+++ prisma.client.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// On définit un objet global pour stocker Prisma (pour éviter les erreurs TS)
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// On utilise l'instance existante ou on en crée une nouvelle
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"], // Optionnel : affiche les requêtes SQL dans ton terminal
+  });
+
+// Si on est pas en production, on stocke l'instance dans le global
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
