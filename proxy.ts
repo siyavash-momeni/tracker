7dc499813fbf1f878f106af6290465a9c6825864
siyavash-momeni <siyavash.mmn@gmail.com>
2026-02-18 16:24:29 +0100

    fix : modification du middleware pour debug

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,21 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+// 1. On crée un "matcher" pour les routes qui doivent être publiques
+// Remplace bien '/api/webhook' par le chemin exact de ton fichier
+const isPublicRoute = createRouteMatcher(['/api/webhook(.*)'])
+
+export default clerkMiddleware(async (auth, request) => {
+  // 2. Si la route n'est PAS publique, on demande une authentification
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
-    // toutes les pages et routes sauf fichiers statiques
-    '/((?!_next|favicon.ico).*)',
-    // toutes les API
-    '/api/(.*)',
-    '/trpc/(.*)',
+    // Ignore les fichiers internes Next.js et les fichiers statiques
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Toujours exécuter pour les routes API (mais le code ci-dessus gérera l'exception)
+    '/(api|trpc)(.*)',
   ],
 }
