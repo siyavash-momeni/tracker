7ece8e70a5227128940ac9131ee5e1c8dfdfb3fd
siyavash-momeni <siyavash.mmn@gmail.com>
2026-02-19 16:45:59 +0100

    feat : upgrade home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black font-sans">
-      <main className="text-center p-8 bg-white dark:bg-zinc-900 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-black dark:text-white mb-4">
-          Bienvenue sur TrackerFront
-        </h1>
-        <p className="text-lg text-zinc-700 dark:text-zinc-300">
-          Site en production
-        </p>
-      </main>
-    </div>
+    // Zone totalement vide, pas de texte ni de cartes.
+    // Le conteneur prend toute la place disponible.
+    <div className="h-full w-full" />
   );
 }
